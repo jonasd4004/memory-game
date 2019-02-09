5dcc50429d24d48a79c6f3bc3fb5f89b37e79567
Jonas Davidow <jonas.davidow@gmail.com>
2019-02-09 09:45:56 -0600

    Updates

---
+++ precache-manifest.47ce08d5f0959e6146f6441dc32db6c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d07f97f772d9ea6319f9",
+    "url": "/memory-game/static/css/main.0fc0fa68.chunk.css"
+  },
+  {
+    "revision": "d07f97f772d9ea6319f9",
+    "url": "/memory-game/static/js/main.d07f97f7.chunk.js"
+  },
+  {
+    "revision": "6cc3fba143227803f814",
+    "url": "/memory-game/static/js/1.6cc3fba1.chunk.js"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "498dc58ab93a8795e35f108796e319bf",
+    "url": "/memory-game/index.html"
+  }
+];
